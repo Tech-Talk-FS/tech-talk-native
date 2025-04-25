426084f2735bfc7b6488d87eb3324cc0bd02cc32
Brad Beltowski <bradbeltowskigmail.com>
2025-04-25 16:45:58 -0400

    Tailwind examples on the config and how to use them.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#023047",
+        light: {
+          100: "#8ECAE6"
+          200: "#219EBC"
+        }
+      },
+    },
   },
   plugins: [],
 };
