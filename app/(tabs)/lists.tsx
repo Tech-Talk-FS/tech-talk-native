c18ecdd1a0faac22b0144bc55c530f3b8a584b28
Brad Beltowski <bradbeltowskigmail.com>
2025-04-25 18:01:36 -0400

    This might suck, but we will learn from it.

--- app/(tabs)/lists.tsx
+++ app/(tabs)/lists.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
 
-const SingleList = () => {
+const Lists = () => {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-primary">Welcome to Tech Talk</Text>
     </View>
   );
 };
-export default SingleList;
+export default Lists;
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,10 @@
 import { Text, View } from "react-native";
 
-const Onboarding = () => {
+const Profile = () => {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-primary">Welcome to Tech Talk</Text>
     </View>
   );
 };
-export default Onboarding;
+export default Profile;
