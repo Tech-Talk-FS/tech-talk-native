b0cae08f410dc8de173d9b1ee0d07a96f1b5c1bd
Kasador <hunterstevenshaw@gmail.com>
2025-04-25 18:49:46 -0700

    Map loaded and configured on map tab.

--- app/(tabs)/mapbox.tsx
+++ app/(tabs)/mapbox.tsx
@@ -1,10 +1,21 @@
-import { Text, View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import MapView from 'react-native-maps';
 
 const Lists = () => {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-primary">Map</Text>
+      <MapView style={styles.map}/>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 export default Lists;
