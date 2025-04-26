cdb3d5ee7e31aa259b4361209dd63be8b8617f68
Brad Beltowski <bradbeltowskigmail.com>
2025-04-25 23:00:36 -0400

    new area

--- app/(tabs)/mapview.tsx
+++ app/(tabs)/mapview.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Dimensions } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 
 const MapDisplay = () => {
@@ -41,7 +41,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    width: "100%",
-    height: "100%",
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
